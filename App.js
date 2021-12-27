08b13975e04e1b7d599223d49e2094958a57d0b7
Saziye <yilmazersaziye@gmail.com>
2021-12-27 20:07:37 +0300

    Redux and axios library added

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import Navigator from './src/navigation';
+import store from './src/store';
 
-const App: () => Node = () => {
-  return <Navigator />;
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 };
 
 export default App;
