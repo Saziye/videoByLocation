c085c81c5a3fd1a5b4a2df7af938e6b7ca0a1447
Saziye <yilmazersaziye@gmail.com>
2022-01-02 18:19:35 +0300

    map screen header hidden

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MapScreen from '../screens/MapScreen';
 
 const Stack = createNativeStackNavigator();
 
 function Navigator() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Map" component={MapScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-  
-  export default Navigator;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigator;
