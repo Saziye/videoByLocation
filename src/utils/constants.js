179f49017b1d589778b17957edbb1f044b6a58e0
Saziye <yilmazersaziye@gmail.com>
2022-01-02 18:20:20 +0300

    google api key added

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = 'AIzaSyDSr3_vWOwwiSyO1x1OsuU-1L4YoBrkDzQ';
+
+export {GOOGLE_API_KEY};
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+import {GOOGLE_API_KEY} from './constants';
+
+export {GOOGLE_API_KEY};
