9da9754140dfefbc0e5b4b7d36af4a754ce61724
Saziye <yilmazersaziye@gmail.com>
2022-01-02 18:19:57 +0300

    youtube api services added

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,28 @@
 import axios from 'axios';
+import {GOOGLE_API_KEY} from '../utils';
+
+export const fetchVideosService = payload => {
+  var config = {
+    method: 'get',
+    url: `https://youtube.googleapis.com/youtube/v3/search?location=${
+      payload.payload.coords.latitude + ',' + payload.payload.coords.longitude
+    }&locationRadius=10km&maxResults=${
+      payload.payload.maxResults
+    }&type=video&key=${GOOGLE_API_KEY}`,
+    headers: {
+      Accept: 'application/json',
+      Authorization: `client:auth2 ${GOOGLE_API_KEY}`,
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        reject(error);
+        console.log({error});
+      });
+  });
+};
