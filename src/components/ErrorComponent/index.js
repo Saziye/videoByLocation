923a06bedb16e4f3f4b21ed9aaabb64dc9cee694
Saziye <yilmazersaziye@gmail.com>
2022-01-04 23:29:51 +0300

    added style in error component

--- src/components/ErrorComponent/index.js
+++ src/components/ErrorComponent/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 
 export default function ErrorComponent({error}) {
     return (
-        <View>
+        <View style={{flex:1, width:'100%',height:'100', justifyContent:'center'}}>
             <Text>{error.message}</Text>
         </View>
     )
